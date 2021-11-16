1fffe6c3369b885cfefee934c9f0f99ecc841485
gaben54 <79724043+gaben54@users.noreply.github.com>
2021-11-16 17:43:26 -0600

    Added basic form layout (#9)
    Co-authored-by: canismajor88 <abstfact87@gmail.com>

--- 456-UI-Ux/DyslexiaReader/src/app/app.module.ts
+++ 456-UI-Ux/DyslexiaReader/src/app/app.module.ts
@@ -3,7 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormComponent } from './form/form.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { HomeScreenComponent } from './home-screen/home-screen.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import { MatIconModule} from "@angular/material/icon";
@@ -12,7 +18,19 @@ import { MatIconModule} from "@angular/material/icon";
 @NgModule({
   declarations: [
     AppComponent,
+
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatFormFieldModule,
+    MatCardModule,
+    FlexLayoutModule
+
     HomeScreenComponent
+
   ],
     imports: [
         BrowserModule,
