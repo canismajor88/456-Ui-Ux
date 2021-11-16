d3c6d8ad63ccc2190430bd274ed6526f7ccc42e5
canismajor88 <abstfact87@gmail.com>
2021-11-16 17:39:16 -0600

    made frame for our form

--- 456-UI-Ux/DyslexiaReader/src/app/app.module.ts
+++ 456-UI-Ux/DyslexiaReader/src/app/app.module.ts
@@ -4,16 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { MatIconModule} from "@angular/material/icon";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeScreenComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NoopAnimationsModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NoopAnimationsModule,
+        MatToolbarModule,
+        MatIconModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
