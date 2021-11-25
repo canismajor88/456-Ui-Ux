c45074a5ae439f70aae2e446cd0a877296b75ace
canismajor88 <abstfact87@gmail.com>
2021-11-25 15:56:21 -0600

    got tts working (#13)
    * got tts working
    
    * added play button

--- 456-UI-Ux/DyslexiaReader/src/app/form/form.component.ts
+++ 456-UI-Ux/DyslexiaReader/src/app/form/form.component.ts
@@ -12,11 +12,33 @@ export class FormComponent implements OnInit {
 
   textForm!: FormGroup;
   error = '';
-
+  synth:any
+  voices:any
   constructor(
-    private formBuilder: FormBuilder,
-    public dialog: MatDialog
-  ) {}
+    private formBuilder: FormBuilder, public dialog: MatDialog)   {
+    this.synth= window.speechSynthesis;
+      }
+  getVoices(){
+    if(this.synth.onvoiceschanged !== undefined){
+      this.voices= this.synth.getVoices();
+      this.synth.onvoiceschanged =this.voices
+    }
+  }
+
+  speak(){
+    this.getVoices()
+    this.synth.cancel()
+    if(this.textForm.value==""){
+      //nothing to say
+      return;
+    }
+    let speakText= new SpeechSynthesisUtterance(this.textForm.value.text);
+    let voice= this.voices[0]
+    speakText.voice=voice
+    speakText.rate=1;
+    speakText.pitch=1;
+    this.synth.speak(speakText)
+  }
 
   ngOnInit(): void {
     this.textForm = this.formBuilder.group({
@@ -44,11 +66,16 @@ export class FormComponent implements OnInit {
 
     this.dialog.open(OutputTextComponent, dialogConfig);
 
-    this.textForm.reset();
+  }
+  pause(){
+    this.synth.pause()
   }
 
   get text() {
     return this.textForm.get('text');
   }
 
+  play() {
+    this.synth.resume
+  }
 }
